2b10165941cc065ef8c7b26aeb07fd9c2fa9640b
Falz4i <naufalzainuzaki@webmail.umm.ac.id>
2024-10-22 10:32:37 +0700

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 
          public class Main {
  public static void main(String[] args) {
- String text = "Hello, Maven!";
+ String text = "Samlekum mamankkk!";
  String reversedText = StringUtils.reverse(text);
  System.out.println(reversedText);
 }
